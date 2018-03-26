7b6435c012ca30183c0304e2a88e0d2f2771c24e
Patrick Michel <patrickmichel96@msn.com>
2018-03-26 21:38:19 +0200

    Commit all before create new project as client for network programming

---
+++ Blockchain.NET.Blockchain/Network/Communication/PushBlockRequest.cs
@@ -0,0 +1,23 @@
+﻿using Blockchain.NET.Core.Mining;
+using Network.Packets;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blockchain.NET.Blockchain.Network.Communication
+{
+    public class PushBlockRequest : RequestPacket
+    {
+        public PushBlockRequest()
+        {
+
+        }
+
+        public PushBlockRequest(Block block)
+        {
+            Block = block;
+        }
+
+        public Block Block { get; set; }
+    }
+}
